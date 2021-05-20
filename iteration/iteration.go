3f6186125e750c8cb4ea286c70b75f3d9dbb3695
Arno Schutijzer <arno.schutijzer@protonmail.com>
2021-05-20 17:22:13 +0200

    use string std

--- iteration/iteration.go
+++ iteration/iteration.go
@@ -1,10 +1,7 @@
 package iteration
 
+import "strings"
+
 func Repeat(sequence string, count int) string {
-	// zero-valued init
-	var repeated string
-	for i := 0; i < count; i++ {
-		repeated += sequence
-	}
-	return repeated
+	return strings.Repeat(sequence, count)
 }
