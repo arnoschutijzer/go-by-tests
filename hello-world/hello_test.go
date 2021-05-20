aa22478a7a3475fe972a4825d453e1db7d917abe
Arno Schutijzer <arno.schutijzer@skryv.com>
2021-05-20 16:14:44 +0200

    hello me

--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -3,10 +3,28 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello()
-	want := "Hello, world"
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
+	assertCorrectMessage := func(t testing.TB, got, want string) {
+		// report the line number of the source code
+		// and not from inside this helper.
+		t.Helper()
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
 	}
+
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Arno")
+		want := "Hello, Arno"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("saying hello to the world", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, world"
+
+		assertCorrectMessage(t, got, want)
+	})
 }
