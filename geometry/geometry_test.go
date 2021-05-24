d201e4826860b2c02342e79ba371dc0947e938db
Arno Schutijzer <arno.schutijzer@protonmail.com>
2021-05-24 22:51:56 +0200

    give tests some meaningful name

--- geometry/geometry_test.go
+++ geometry/geometry_test.go
@@ -34,17 +34,20 @@ func TestArea(t *testing.T) {
 
 	// see https://github.com/golang/go/wiki/TableDrivenTests
 	areaTests := []struct {
-		shape Shape
-		want  float64
+		name    string
+		shape   Shape
+		hasArea float64
 	}{
-		{shape: Rectangle{Height: 10.0, Width: 10.0}, want: 100.0},
-		{shape: Rectangle{Height: 10.0, Width: 6.0}, want: 60.0},
-		{shape: Circle{Radius: 10.0}, want: 314.1592653589793},
-		{shape: Triangle{Base: 12, Height: 6}, want: 36.0},
+		{name: "Square", shape: Rectangle{Height: 10.0, Width: 10.0}, hasArea: 100.0},
+		{name: "Rectangle", shape: Rectangle{Height: 10.0, Width: 6.0}, hasArea: 60.0},
+		{name: "Circle", shape: Circle{Radius: 10.0}, hasArea: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{Base: 12, Height: 6}, hasArea: 36.0},
 	}
 
 	for _, tt := range areaTests {
-		got := tt.shape.Area()
-		assert.Equal(t, tt.want, got)
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			assert.Equal(t, tt.hasArea, got)
+		})
 	}
 }
